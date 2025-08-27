9134ac970148e7f91b84d10ed7311c8fb1dcf9ef
Fabrizio <fabriziodisciorio91@gmail.com>
2025-08-27 11:22:27 +0200

    Update download_data.py

--- modules/download_data.py
+++ modules/download_data.py
@@ -1,11 +1,17 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
 import os
+import streamlit as st
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 def download_from_kaggle(dataset: str, path: str = "data"):
     if not os.path.exists(path):
         os.makedirs(path)
+    
+    # Setta le credenziali come variabili d'ambiente
+    os.environ["KAGGLE_USERNAME"] = st.secrets["kaggle"]["username"]
+    os.environ["KAGGLE_KEY"] = st.secrets["kaggle"]["key"]
 
     api = KaggleApi()
     api.authenticate()
+
     api.dataset_download_files(dataset, path=path, unzip=True)
-    print(f"✅ Dataset {dataset} scaricato in {path}")
+    st.write(f"✅ Dataset `{dataset}` scaricato correttamente in `{path}`.")
