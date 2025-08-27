4e68db75babda4b2e118e6d3e6542176c3fee388
Fabrizio <fabriziodisciorio91@gmail.com>
2025-08-27 11:07:36 +0200

    Update download_data.py

--- modules/download_data.py
+++ modules/download_data.py
@@ -1,18 +1,11 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
 import os
-import subprocess
 
 def download_from_kaggle(dataset: str, path: str = "data"):
-    """
-    Scarica un dataset da Kaggle nella cartella indicata.
-    Richiede la CLI 'kaggle' configurata correttamente con API key.
-    """
     if not os.path.exists(path):
         os.makedirs(path)
 
-    print(f"Scaricando il dataset {dataset}...")
-    subprocess.run([
-        "kaggle", "datasets", "download", "-d", dataset,
-        "-p", path, "--unzip"
-    ], check=True)
-    print(f"Dataset {dataset} scaricato e decompresso in '{path}'.")
-
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files(dataset, path=path, unzip=True)
+    print(f"✅ Dataset {dataset} scaricato in {path}")
